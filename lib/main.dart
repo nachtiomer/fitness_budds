9b312c99cd70bec9616b4227d1106b6864cf309d
YanivGalron12 <63013969+YanivGalron12@users.noreply.github.com>
2020-04-13 17:49:09 +0300

    prhvhhv

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,7 @@ import 'package:fitnessbudds/splash.dart';
 
 //
 void main() => runApp(MaterialApp(
-      theme: ThemeData(
-          primaryColor: Colors.amber, accentColor: Colors.amber),
+      theme: ThemeData(primaryColor: Colors.amber, accentColor: Colors.amber),
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     ));
-
